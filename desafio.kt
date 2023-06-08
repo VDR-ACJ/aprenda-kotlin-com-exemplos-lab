f30553b4be653966bf0364eeb15945561b74c731
VDR-ACJ <57298002+VDR-ACJ@users.noreply.github.com>
2023-06-08 18:12:32 -0300

    Exemplo de cadastro de formacoes, conteudo e inscritos

--- desafio.kt
+++ desafio.kt
@@ -1,32 +1,12 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-/*enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
-}
-
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}*/
-
-//*enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+enum class Nivel (val printableName: String){ BASICO("BASICO"), INTERMEDIARIO("INTERMEDIARIO"), AVANCADO("AVANCADO") }
 
 //data class usuario e email
 data class Usuario(val nome: String, val email: String )
 
 //nome do conteudo e duracao
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: String)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
@@ -41,26 +21,29 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 }
 
 fun main() {
+    
     //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
     //var conteudos:List<ConteudoEducacional> = ConteudoEducacional("Primeiros passos",60)
-    //val conteudos = mutableListOf("A",60)
-    //var conteudoEducacional = ConteudoEducacional("A",60)
-    val ad1 = ConteudoEducacional("A",60)
-    val ad2 = ConteudoEducacional("B",30)
-    val ads = listOf<ConteudoEducacional>(ad1, ad2)
-    val formacao = Formacao(nome="Kotlin",ads)
+    
+    val conteudo1 = ConteudoEducacional("Fase A", 60, Nivel.valueOf("BASICO").printableName)
+    val conteudo2 = ConteudoEducacional("Fase B", 30, Nivel.valueOf("AVANCADO").printableName)
+    val conteudos = listOf<ConteudoEducacional>(conteudo1, conteudo2)
+    val formacao = Formacao(nome="Kotlin",conteudos)
+    val formacao2 = Formacao(nome="Flutter",conteudos)
+    
     val usuario1 = Usuario("Jose","jose@dio.me")
+    val usuario2 = Usuario("Maria","maria@dio.me")
+    
     formacao.matricular(usuario1)
-    println(formacao) // interests: ["A,B,C"]
-    println(formacao.inscritos) // interests: ["A,B,C"]
+    formacao.matricular(usuario2)
+    
+    formacao2.matricular(usuario1)
+    
+    println(formacao) // Formacao Kotlin
+    println(formacao.inscritos) // Inscritos na formacao kotlin
+    
+    println(formacao2) // Formacao Flutter 
+    println(formacao2.inscritos) // Inscritos na formacao kotlin
 
-    
-    //ConteudoEducacional("Desafios",60)
-    
-    //var conteudoA = ConteudoEducacional
-    //val formacaoA = Formacao("Kotlin", conteudoA)
-    //formacaoA.matricular("Jose","jose@dio.me")
-	//val formacaoA = Formacao("Kotlin",conteudos) 
-    println("teste")
 }
